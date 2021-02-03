741c82a19afc7d37087b34f30dd416615d512367
melserngawy <melserng@redhat.com>
2021-02-03 11:46:38 -0500

    Fix typo
    Signed-off-by: melserngawy <melserng@redhat.com>

--- site_gen.sh
+++ site_gen.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 CLUSTER_NAME=$1
-profile=$2
+PROFILE=$2
 RAN_MANIFEST_REPO=$3
 ORG_RAN_MANIFEST_REPO=https://github.com/redhat-ztp/ztp-ran-manifests
 
-if [ $profile == "cu" ]; then
+if [ $PROFILE == "cu" ]; then
   cp -rf subscriptions/sites/sample_site_cu_subscription subscriptions/sites/${CLUSTER_NAME}_subscription
   sed -i "s/sample-site-cu/$CLUSTER_NAME/g" subscriptions/sites/${CLUSTER_NAME}_subscription/*
-elif [ $profile == "du" ]; then
+elif [ $PROFILE == "du" ]; then
   cp -rf subscriptions/sites/sample_site_du_subscription subscriptions/sites/${CLUSTER_NAME}_subscription
   sed -i "s/sample-site-du/$CLUSTER_NAME/g" subscriptions/sites/${CLUSTER_NAME}_subscription/*
 else
-  echo "Profile should be either cu or du. Profile=" $profile
+  echo "Profile should be either cu or du. Profile=" $PROFILE
   exit 0
 fi
 

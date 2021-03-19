46edb5bca6bbd2a1973ea4102a429675bf6faca6
Ian Miller <imiller@redhat.com>
2021-03-19 17:33:09 -0400

    Add support to site_gen for specifying the branch name. This allows support for testing to use different branches per test run if/as needed.

--- site_gen.sh
+++ site_gen.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 RAN_MANIFEST_REPO=$1
+BRANCH=${2:-main}
 ORG_RAN_MANIFEST_REPO=https://github.com/redhat-ztp/ztp-ran-manifests
 
 if [ ! -z $RAN_MANIFEST_REPO ]; then
   for f in $(find subscriptions/ -name *.yaml); do
     #sed --debug -i "s,$ORG_RAN_MANIFEST_REPO,$RAN_MANIFEST_REPO,g" $f
     sed -i "s,$ORG_RAN_MANIFEST_REPO,$RAN_MANIFEST_REPO,g" $f
+    sed -i "s,^\( *apps.open-cluster-management.io/git-branch:\).*,\1 ${BRANCH},g" $f
   done
 fi
